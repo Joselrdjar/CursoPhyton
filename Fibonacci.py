9bae03773b1365bee92ad269722fbb4a64819c54
Joselrdjar <joselrd@gmail.com>
2024-10-27 08:03:34 -0500

    Fibo

---
+++ Fibonacci.py
@@ -0,0 +1,12 @@
+def serie(limite):
+    a = 0
+    b = 1
+    while a<limite:
+        yield a
+        a = b
+        b = a+b
+        
+    for numeros in serie(10):
+        print (numeros)
+    
+    
